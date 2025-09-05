6cbd794975f31a90c8e6e69de61120c6762b6504
kingsley388 <kingsleyuzokwe0@gmail.com>
2025-09-05 12:56:50 +0100

    Delete FootyFanHub directory

--- FootyFanHub/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://kingsley:yourpassword@localhost/footyfanhub'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- FootyFanHub/models.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-class NewsPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    date_posted = db.Column(db.DateTime, default=db.func.current_timestamp())
